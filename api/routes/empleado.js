f70fdfffef87a3cd542bd205a9f309e3d8d6d21d
Fersoher <74140565+Fersoher@users.noreply.github.com>
2023-10-21 12:54:28 +0100

    Empleados completo

--- api/routes/empleado.js
+++ api/routes/empleado.js
@@ -1,15 +1,23 @@
 const router = require('express').Router()
-const { getAllEmpleados, getOneEmpleado, createEmpleado, updateEmpleado, deleteEmpleado, getOwnTests, getOwnAsignatura, getOwnCursos } = require('../controllers/empleado')
+const { getAllEmpleados,
+	getOwnProfile,
+	getOwnTests,
+    getOwnAsignaturas,
+    getOwnCursos,
+	getOneEmpleado,
+	createEmpleado,
+	updateEmpleado,
+	updateOwnEmpleado,
+	deleteEmpleado } = require('../controllers/empleado')
 
 router.get('/empleado', getAllEmpleados)
 router.get('/empleado/:empleadoId', getOneEmpleado)
 router.get('/empleado/profile', getOwnProfile)
 router.get('/empleado/tests', getOwnTests)
-router.get('/empleado/asignatura', getOwnAsignatura)
+router.get('/empleado/asignatura', getOwnAsignaturas)
 router.get('/empleado/curso', getOwnCursos)
-router.get('/empleado/test', getAvaibleTest)
 router.post('/empleado', createEmpleado)
-router.put('/empleado/profile', updateEmpleado)
+router.put('/empleado/profile', updateOwnEmpleado)
 router.put('/empleado/:empleadoId', updateEmpleado)
 router.delete('/empleado/:empleadoId', deleteEmpleado)
 
