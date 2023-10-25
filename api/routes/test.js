4883d50a646269af11a0969eebe53f3a0ca0f9f8
Fersoher <fernandodarius1996.25@gmail.com>
2023-10-25 11:37:44 +0100

    permisos

--- api/routes/test.js
+++ api/routes/test.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
 const { getAllTests, getOneTest, createTest, updateOwnTest, updateTest, deleteTest } = require('../controllers/test.controller')
-const { checkUser, checkAdmin } = require('../middlewares/auth')
+const { checkUser, checkAdmin, checkEmpleado } = require('../middlewares/auth')
 
 router.get('/', checkUser, checkAdmin, getAllTests)
 router.get('/:id', checkUser, checkAdmin, getOneTest)
-router.post('/', checkUser, checkAdmin, createTest)
-router.put('/avaible', checkUser, checkAdmin, updateOwnTest)
+router.post('/', checkUser, checkEmpleado, createTest)
+router.put('/avaible', checkUser, checkEmpleado, updateOwnTest)
 router.put('/:id', checkUser, checkAdmin, updateTest)
-router.delete('/:id', checkUser, checkAdmin, deleteTest)
+router.delete('/:id', checkUser, checkEmpleado, deleteTest)
 
 module.exports = router
